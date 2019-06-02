79c28bbefb78cff04dd67d54a51628682919b8c8
Matt Wilkinson <matthew.wilkinson@gmail.com>
2019-06-02 19:27:56 +0100

    Skeleton for C# compiler

---
+++ NetRegexCompiler/Compiler/Text/RegularExpressions/RegexCSharpCompiler.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace NetRegexCompiler.Compiler.Text.RegularExpressions
+{
+    internal sealed class RegexCSharpCompiler
+    {
+        private TextWriter Writer { get; }
+        private RegexCode Code { get; }
+        private int[] Codes { get; }
+        private string[] Strings { get; }
+        private RegexPrefix? FirstCharacterPrefix { get; }
+        private RegexBoyerMoore BoyerMoorePrefix { get; }
+        private int Anchors { get; }
+        private int TrackCount { get; }
+        private RegexOptions Options { get; }
+
+        private RegexCSharpCompiler(TextWriter writer, RegexCode code, RegexOptions options)
+        {
+            Writer = writer;
+            Code = code;
+            Codes = code.Codes;
+            Strings = code.Strings;
+            FirstCharacterPrefix = code.FCPrefix;
+            BoyerMoorePrefix = code.BMPrefix;
+            Anchors = code.Anchors;
+            TrackCount = code.TrackCount;
+            Options = options;
+        }
+
+        public static void GenerateCSharpCode(TextWriter writer, RegexCode code, RegexOptions options)
+        {
+            var codeGenerator = new RegexCSharpCompiler(writer, code, options);
+
+        }
+    }
+}
