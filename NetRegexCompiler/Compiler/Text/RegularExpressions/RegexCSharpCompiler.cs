861dcbc89f2b86b2064316063904c2851b8d0df3
Matt Wilkinson <matthew.wilkinson@gmail.com>
2019-08-02 12:52:30 +0100

    Skeleton dode for generating the three override methods, and base class field declarations

--- NetRegexCompiler/Compiler/Text/RegularExpressions/RegexCSharpCompiler.cs
+++ NetRegexCompiler/Compiler/Text/RegularExpressions/RegexCSharpCompiler.cs
@@ -39,8 +39,62 @@ namespace NetRegexCompiler.Compiler.Text.RegularExpressions
 
         public static void GenerateCSharpCode(TextWriter writer, RegexCode code, RegexOptions options)
         {
-            var codeGenerator = new RegexCSharpCompiler(writer, code, options);
+            using(var codeGenerator = new RegexCSharpCompiler(writer, code, options))
+                codeGenerator.Generate();
+        }
 
+        private void Generate()
+        {
+            Writer.Using("System.Diagnostics");
+            Writer.Using("System.Globalization");
+            using (Writer.Namespace("NetRegexCompiler.Compiler.Text.RegularExpressions"))
+            using (Writer.Type("public sealed class CompiledRegexRunnerFactory : RegexRunnerFactory"))
+            {
+                Writer.Write($"protected internal override RegexRunner CreateInstance() => new CompiledRegexRunner();");
+                using (Writer.Type("private sealed class CompiledRegexRunner : RegexRunner"))
+                {
+                    GenerateInitTrackCount();
+                    GenerateFindFirstChar();
+                    GenerateGo();
+                }
+            }
+        }
+
+        private readonly Field runtextbeg    = Field.Parse("runtextbeg");
+        private readonly Field runtextend    = Field.Parse("runtextend");
+        private readonly Field runtextstart  = Field.Parse("runtextstart");
+        private readonly Field runtext       = Field.Parse("runtext");
+        private readonly Field runtextpos    = Field.Parse("runtextpos");
+        private readonly Field runtrack      = Field.Parse("runtrack");
+        private readonly Field runtrackpos   = Field.Parse("runtrackpos");
+        private readonly Field runstack      = Field.Parse("runstack");
+        private readonly Field runstackpos   = Field.Parse("runstackpos");
+        private readonly Field runcrawl      = Field.Parse("runcrawl");
+        private readonly Field runcrawlpos   = Field.Parse("runcrawlpos");
+        private readonly Field runtrackcount = Field.Parse("runtrackcount");
+        private readonly Field runmatch      = Field.Parse("runmatch");
+        private readonly Field runregex      = Field.Parse("runregex");
+
+        private void GenerateInitTrackCount()
+        {
+            using (Writer.Method("protected override void InitTrackCount()"))
+                Writer.Write($"{runtrackcount} = {Code.TrackCount};");
+        }
+
+        private void GenerateFindFirstChar()
+        {
+            using (Writer.Method("protected override bool FindFirstChar()"))
+            {
+
+            }
+        }
+
+        private void GenerateGo()
+        {
+            using (Writer.Method("protected override void Go()"))
+            {
+                
+            }
         }
     }
 }
