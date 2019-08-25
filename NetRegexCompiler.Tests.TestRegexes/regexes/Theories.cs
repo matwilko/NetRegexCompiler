0f3b9a9a52728603d9836976983c9402d3bac32e
Matt Wilkinson <matthew.wilkinson@gmail.com>
2019-08-25 18:03:51 +0100

    Include blank Theories.cs for initial build, and update projects to use latest C# version

---
+++ NetRegexCompiler.Tests.TestRegexes/regexes/Theories.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace NetRegexCompiler.Tests.CompiledTestRegexes
+{
+    class Theories
+    {
+        public static IEnumerable<object[]> RegexTests()
+        {
+            yield break;
+        }
+    }
+}
